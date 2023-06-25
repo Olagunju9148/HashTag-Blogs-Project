156889fa8758909a1b3e3e59b5bf702e8f4203e6
ItsJustMe41 <65431481+Olagunju9148@users.noreply.github.com>
2023-06-25 22:07:15 +0100

    commit

---
+++ nav.js
@@ -0,0 +1,10 @@
+const navLinks = document.querySelector(".nav-links")
+const menuNavBar = document.querySelector(".menu-bar")
+
+function menuBar () {
+    menuBar = "clicked"
+    if(menuBar = "clicked") {
+        navLinks.
+    }
+    console.log("clicked");
+}
